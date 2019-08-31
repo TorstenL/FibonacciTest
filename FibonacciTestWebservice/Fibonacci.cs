047ba610382c83bcf3c0147b993743bd6e02cd33
Torsten Lehnert <Mail@TLehnert.de>
2019-08-31 10:27:24 +0200

    Fibonacci implementiert

--- FibonacciTestWebservice/Fibonacci.cs
+++ FibonacciTestWebservice/Fibonacci.cs
@@ -4,9 +4,27 @@ namespace FibonacciTestWebservice
 {
     public class Fibonacci
     {
+        //https://www.nayuki.io/page/fast-fibonacci-algorithms
+        private static BigInteger FastDoublingFibonacci(BigInteger n)
+        {
+            if (n.Equals(0))
+            {
+                return BigInteger.Zero;
+            }else if (n <= 2)
+            {
+                return BigInteger.One;
+            }
+            BigInteger k = n/2;
+            BigInteger a = FastDoublingFibonacci(k + 1);
+            BigInteger b = FastDoublingFibonacci(k);
+            return (n % 2 == 1) 
+                ? a * a + b * b 
+                : b * (2 * a - b); 
+        }
+        
         public BigInteger Calculate(long n)
         {
-            throw new System.NotImplementedException();
+            return FastDoublingFibonacci(new BigInteger(n));
         }
     }
 }
